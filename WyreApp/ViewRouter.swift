92de9aac15e90f7ae0820bd5e7690f247f93b20b
Sai Kannekanti <darkcompgeek@gmail.com>
2020-06-27 15:49:42 -0400

    created viewRouter.swift file

---
+++ WyreApp/ViewRouter.swift
@@ -0,0 +1,17 @@
+//
+//  ViewRouter.swift
+//  WyreApp
+//
+//  Created by Sai Kannekanti on 6/27/20.
+//  Copyright © 2020 Sai Kannekanti. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class ViewRouter: ObservableObject {
+    //With the @Published property wrapper, we notify all observing views to update themselves whenever the currentView variable changes.
+    @Published var currentView = "home"
+    
+}
