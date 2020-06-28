7391481224911fc344de00791d3bd17267db915c
Sai Kannekanti <darkcompgeek@gmail.com>
2020-06-28 11:24:09 -0400

    created a Color Manager swift file to manage color variables

---
+++ WyreApp/ColorManager.swift
@@ -0,0 +1,19 @@
+//
+//  ColorManager.swift
+//  WyreApp
+//
+//  Created by Sai Kannekanti on 6/28/20.
+//  Copyright © 2020 Sai Kannekanti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ColorManager {
+    // create static variables for custom colors
+    static let wyrePurple = Color("wyrePurple")
+    static let wyreDarkPurple = Color("wyreDarkPurple")
+    static let wyreGreen = Color("wyreGreen")
+    static let wyreGray = Color("wyreGray")
+    //... add the rest of your colors here
+}
+ 
