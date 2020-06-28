795c2352386e0d52fa7ddae4ddb018fb2606b87d
Sai Kannekanti <darkcompgeek@gmail.com>
2020-06-28 16:15:51 -0400

    fixed spacing issues in Home Screen view in SwiftUI. Now everything is edge to edge and touches each other.

--- WyreApp/ContentView.swift
+++ WyreApp/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     var body: some View {
         
         GeometryReader { geometry in
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 0.0) {
             //This is the Content in the main view
                 ZStack {
                     Rectangle()
@@ -35,7 +35,7 @@ struct ContentView: View {
                         
                 }
                 }
-                HStack{
+                HStack(alignment: .center, spacing: 0.0){
                     Button(action: {
                         print("Hello button tapped!")
                     }) {
@@ -82,33 +82,35 @@ struct ContentView: View {
                     RoundedRectangle(cornerRadius: 15)
                         .fill(Color.white)
                         .shadow(color: .gray, radius: 20.0, x: 20, y: 10)
-                HStack{
-//Home Tab
-                    
-                    Image("Home")
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: geometry.size.width/3, height: 75)
-//New Wyre Button
-                    Button(action: {
-                        print("Hello button tapped!")
-                    }) {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 100)
-                                .fill(ColorManager.wyrePurple)
-                                .padding(10)
-                                
-                            Image("Wyre Icon")
-                                .foregroundColor(Color.white)
-                        }
-                        }
-//Me Tab
-                    Image("Person")
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: geometry.size.width/3, height: 75)
-                      }
+                    VStack{
+                        HStack(spacing: 0.0){
+         //Home Tab
+                             Image("Home")
+                                 .aspectRatio(contentMode: .fit)
+                                 .frame(width: geometry.size.width/3, height: 75)
+         //New Wyre Button
+                             Button(action: {
+                                 print("Hello button tapped!")
+                             }) {
+                                 ZStack{
+                                     RoundedRectangle(cornerRadius: 100)
+                                         .fill(ColorManager.wyrePurple)
+                                         .padding(10)
+                                         
+                                     Image("Wyre Icon")
+                                         .foregroundColor(Color.white)
+                                 }
+                                 }
+         //Me Tab
+                             Image("Person")
+                                 .aspectRatio(contentMode: .fit)
+                                 .frame(width: geometry.size.width/3, height: 75)
+                               }
+                        Spacer()
+                    }
                 }
-
                 .frame(width: geometry.size.width, height: geometry.size.height/9.6)
+                
                 }.edgesIgnoringSafeArea(.bottom)
             }
         }
