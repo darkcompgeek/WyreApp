48f5d21255637bbaff7e502e66f4e91e67ede54c
Sai Kannekanti <darkcompgeek@gmail.com>
2020-06-29 16:48:29 -0400

    replaced black circles in Feed LIst View with actual profile pictures, and imported profile pictures from unsplashto project

--- WyreApp/FeedList.swift
+++ WyreApp/FeedList.swift
@@ -13,13 +13,12 @@ struct PostContent{
     var caption: String
     var time: String
     var likeCount: Int
-
 }
 
 struct FeedList: View {
     //each feed post that goes in the list
     var postList = [
-//        PostContent(title: "You paid Dalia", caption: "amazing", time: "2m", likeCount: 1),
+        PostContent(title: "You paid Dalia", caption: "amazing", time: "2m", likeCount: 1),
         PostContent(title: "Sai paid Dalia", caption: "amazing", time: "10m", likeCount: 2),
         PostContent(title: "Sai paid Dalia", caption: "amazing", time: "2m", likeCount: 1),
         PostContent(title: "Sai paid Dalia", caption: "amazing", time: "2m", likeCount: 1),
@@ -40,28 +39,38 @@ struct FeedList: View {
                         Image(systemName: "globe")
                         Text("\(PostContent.time)").font(.custom("Gotham-Book", size: 16))
                     }
+                    
                     Spacer().frame(width: 50)
+                    
                     VStack{
                         Text("\(PostContent.title)")
                             .font(.custom("Gotham-Medium", size: 16))
                             .multilineTextAlignment(.center)
+                        
                         HStack{
-                            Circle().frame(height: 40)
+                            Image("001").resizable()
+                                .frame(width: 40, height: 40)
+                            .clipShape(Circle())
                             Rectangle()
                                 .foregroundColor(ColorManager.wyrePurple)
                                 .frame(height: 2)
-                            Circle()
-
+                            Image("001").resizable()
+                                .frame(width: 40, height: 40)
+                            .clipShape(Circle())
                         }
+                        
                         Text("\(PostContent.caption)")
                             .font(.custom("Gotham-Book", size: 16))
                             .multilineTextAlignment(.center)
                     }
+                    
                     Spacer().frame(width: 50)
+                    
                     HStack{
                         Image(systemName: "heart")
                         Text("\(PostContent.likeCount)").font(.custom("Gotham-Book", size: 16))
                     }
+                    
                 }
                 .padding()
         }    }
