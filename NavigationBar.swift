5a39bd3fb83c6d1b67426aee3eea4127ba371320
Sai Kannekanti <darkcompgeek@gmail.com>
2020-06-28 20:14:20 -0400

    separated NavigationBar SwiftUI code into its own swiftUI file in an effort to make this project more modular

---
+++ NavigationBar.swift
@@ -0,0 +1,56 @@
+//
+//  NavigationBar.swift
+//  WyreApp
+//
+//  Created by Sai Kannekanti on 6/28/20.
+//  Copyright © 2020 Sai Kannekanti. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        //this is for the Navigation Bar.
+               ZStack{
+                   RoundedRectangle(cornerRadius: 15)
+                       .fill(Color.white)
+                       .shadow(color: .gray, radius: 20.0, x: 20, y: 10)
+                   VStack(alignment: .center){
+                       HStack(alignment: .center, spacing: 0.0){
+                           Spacer()
+        //Home Tab
+                            Image("Home")
+                                .aspectRatio(contentMode: .fit)
+                               .padding()
+        //New Wyre Button
+                            Button(action: {
+                                print("Hello button tapped!")
+                            }) {
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 100)
+                                        .fill(ColorManager.wyrePurple)
+                                        .padding(10)
+                                       .frame(width: 150, height: 75)
+                                        
+                                    Image("Wyre Icon")
+                                        .foregroundColor(Color.white)
+                                }
+                            }
+                            .padding(.horizontal, 25.0)
+        //Me Tab
+                           Image("Person")
+                                       .aspectRatio(contentMode: .fit)
+                                       .padding()
+                           Spacer()
+                       }
+
+                   }
+               }
+    }
+}
+
+struct NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBar()
+    }
+}
--- WyreApp/ContentView.swift
+++ WyreApp/ContentView.swift
@@ -8,59 +8,73 @@
 
 import SwiftUI
 
-var currentBalance = 25.00
-
 struct ContentView: View {
     @State private var selection = 0
     var body: some View {
-        
-        GeometryReader { geometry in
-            VStack(alignment: .leading, spacing: 0.0) {
-            //This is the Content in the main view
-                    Rectangle().fill(ColorManager.wyrePurple)
-                        .edgesIgnoringSafeArea(.top)
-                        .frame(height: geometry.size.height/4.5)
-                        .overlay(VStack(alignment: .leading){
-                            Spacer()
-                            Text("BALANCE")
-                                .foregroundColor(Color.white)
-                                .multilineTextAlignment(.leading)
-                                .font(.custom("Gotham-Bold", size: 16))
-                                .lineLimit(1)
-                            HStack {
-                                Text("$25.00")
-                                    .foregroundColor(Color.white)
-                                    .multilineTextAlignment(.leading)
-                                    .font(.custom("Gotham-Black", size: 50))
-                                    .lineLimit(1)
-                                Spacer()
-                            }
-                            
-                        }.padding(.bottom, 15.0)
-                        .padding(.leading, 25))
 
-                HStack(alignment: .center, spacing: 0.0){
-                    Button(action: {
-                        print("Hello button tapped!")
-                    }) {
-                        Text("ADD FUNDS")
-                            .foregroundColor(Color.white)
-                            .font(.custom("Gotham-Medium", size: 16))
-                            .padding(20)
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                    }.background(ColorManager.wyreDarkPurple)
-                    Button(action: {
-                        print("Hello button tapped!")
-                    }) {
-                        Text("MOVE TO BANK")
-                            .font(.custom("Gotham-Medium", size: 16))
-                            .foregroundColor(Color.white)
-                            .padding(20)
-                            .frame(minWidth: 0, maxWidth: .infinity)
-                        }
-                        .background(ColorManager.wyreDarkPurple)
-                }
-                
+        GeometryReader { geometry in
+            VStack(alignment: .center, spacing: 0) {
+                //This is the Content in the main view
+                VStack{
+                    HStack{
+                                        Spacer()
+                                        Button(action: {
+                                            print("Hello button tapped!")
+                                        }) {
+                                            Text("Settings")
+                                                .foregroundColor(Color.white)
+                                                .padding(20)
+                                        }
+                    }.background(
+                                        ColorManager.wyrePurple
+                                    )
+                                    VStack(alignment: .leading){
+                                        Spacer()
+                                        HStack{
+                                            Text("BALANCE")
+                                                .foregroundColor(Color.white)
+                                                .multilineTextAlignment(.leading)
+                                                .font(.custom("Gotham-Bold", size: 16))
+                                                .lineLimit(1)
+                                            Spacer()
+                                        }
+                                        HStack {
+                                            Text("$25.00")
+                                                .foregroundColor(Color.white)
+                                                .multilineTextAlignment(.leading)
+                                                .font(.custom("Gotham-Black", size: 50))
+                                                .lineLimit(1)
+                                            Spacer()
+                                        }
+                                    }
+                                        .padding(.bottom, 15.0)
+                                    .padding(.leading, 25)
+                                        .background(ColorManager.wyrePurple)
+                    //ADD FUNDS AND MOVE TO BANK BUTTON ROW
+                                    HStack(alignment: .center, spacing: 0.0){
+                                        Button(action: {
+                                            print("Hello button tapped!")
+                                        }) {
+                                            Text("ADD FUNDS")
+                                                .foregroundColor(Color.white)
+                                                .font(.custom("Gotham-Medium", size: 16))
+                                                .padding(20)
+                                            .frame(minWidth: 0, maxWidth: geometry.size.width)
+                                        }.background(ColorManager.wyreDarkPurple)
+                                        Button(action: {
+                                            print("Hello button tapped!")
+                                        }) {
+                                            Text("MOVE TO BANK")
+                                                .font(.custom("Gotham-Medium", size: 16))
+                                                .foregroundColor(Color.white)
+                                                .padding(20)
+                                                .frame(minWidth: 0, maxWidth: geometry.size.width)
+                                            }
+                                            .background(ColorManager.wyreDarkPurple)
+                                    }
+                }.edgesIgnoringSafeArea(.top)
+                .frame(height: geometry.size.height/4)
+//TAB BAR FOR FEED
                 HStack {
                     Spacer()
                     Spacer()
@@ -72,79 +86,40 @@ struct ContentView: View {
                     Spacer()
                     Spacer()
                 }.padding()
+//FEED LIST
                 HStack(alignment: .center) {
                     List {
                         HStack{
-                            Spacer()
+                            Spacer().frame(width: 50)
                             VStack{
                                 Text("Sai paid Vipin")
                                     .font(.custom("Gotham-Medium", size: 16))
                                     .multilineTextAlignment(.center)
                                 HStack{
-                                    Circle()
-                                    Circle()
+                                    Circle().frame(width:50)
+                                    Rectangle().frame(height: 2)
+                                        .foregroundColor(ColorManager.wyrePurple)
+                                    Circle().frame(width:50)
+
                                 }
                                 Text("Thanks! :)")
                                     .font(.custom("Gotham-Book", size: 16))
                                     .multilineTextAlignment(.center)
                             }
-                            Spacer()
+                            Spacer().frame(width: 50)
                         }.frame(height: geometry.size.height/10)
                         .padding()
                     }
                 }
-//                Text("Balance").font(.custom("Gotham-Bold", size: 30))
-                
-                
-//this is for the Navigation Bar.
-                ZStack{
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(Color.white)
-                        .shadow(color: .gray, radius: 20.0, x: 20, y: 10)
-                        
-                    VStack{
-                        
-                        HStack(alignment: .center, spacing: 0.0){
-                            Spacer()
-         //Home Tab
-                             Image("Home")
-                                 .aspectRatio(contentMode: .fit)
-                                .padding()
-         //New Wyre Button
-                             Button(action: {
-                                 print("Hello button tapped!")
-                             }) {
-                                 ZStack{
-                                     RoundedRectangle(cornerRadius: 100)
-                                         .fill(ColorManager.wyrePurple)
-                                         .padding(10)
-                                        .frame(width: 150, height: 75)
-                                         
-                                     Image("Wyre Icon")
-                                         .foregroundColor(Color.white)
-                                 }
-                             }
-                             .padding(.horizontal, 25.0)
-         //Me Tab
-                            Image("Person")
-                                        .aspectRatio(contentMode: .fit)
-                                        .padding()
-                            Spacer()
-                        }
-                        Spacer()
-                    }
-                }
-                .frame(width: geometry.size.width, height: geometry.size.height/9.6)
-                
                 
                 }.edgesIgnoringSafeArea(.bottom)
             }
         }
-    
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
