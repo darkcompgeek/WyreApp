278c0bad37bd0f5bfa60f3c3402c44d000a55b60
Sai Kannekanti <darkcompgeek@gmail.com>
2020-07-03 11:12:55 -0400

    replaced UINavigation bar in new wyre sheet with custom written navigation stack

--- NewWyreFormSheet.swift
+++ NewWyreFormSheet.swift
@@ -14,46 +14,56 @@ struct NewWyreFormSheet: View {
     
     var body: some View {
         NavigationView{
-            Form{
-                NewWyre_UsernameField().padding(0)
             
-                Section{
-                    NewWyreAmountField()
-                }
-                Section{
-                    NavigationLink(destination: Text("Payment Methods")) {
-                        HStack{
-                            Image(systemName: "lock").font(.system(size: 30, weight: .semibold)).padding()
-                            VStack(alignment: .leading, spacing: 4.0){
-                                Text("Payment Method").font(.custom("Gotham-Bold" ,size: 14))
-                                Text("Wyre Balance").font(.custom("Gotham-Book" ,size: 14))
-                            }
-                        }
-                        }.frame(height: 75)
-                    
-                    
-                    
-                        NavigationLink(destination: Text("Privacy Options")) {
+            VStack(spacing:0.0) {
+                HStack{
+                    Button(action: {
+                        self.mode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "xmark").font(.system(size: 22, weight: .semibold)).foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    Text("Pay").font(.custom("Gotham-Bold", size: 20)).foregroundColor(Color.white).padding()
+                    Text("Request").font(.custom("Gotham-Bold", size: 20)).foregroundColor(Color.white).opacity(0.5).padding()
+                    Spacer()
+                }.padding().frame(height:70).background(ColorManager.wyrePurple)
+                
+                Form{
+                    NewWyre_UsernameField().padding(0)
+                
+                    Section{
+                        NewWyreAmountField()
+                    }
+                    Section{
+                        NavigationLink(destination: Text("Payment Methods")) {
                             HStack{
                                 Image(systemName: "lock").font(.system(size: 30, weight: .semibold)).padding()
                                 VStack(alignment: .leading, spacing: 4.0){
-                                    Text("Privacy Options").font(.custom("Gotham-Bold" ,size: 14))
-                                    Text("Private").font(.custom("Gotham-Book" ,size: 14))
+                                    Text("Payment Method").font(.custom("Gotham-Bold" ,size: 14))
+                                    Text("Wyre Balance").font(.custom("Gotham-Book" ,size: 14))
                                 }
                             }
                             }.frame(height: 75)
-                }
-                Button(action: {print("hello")}) {
-                    Text("Next").font(.custom("Gotham-Bold" ,size: 16)).multilineTextAlignment(.center).padding().foregroundColor(ColorManager.wyrePurple)
-                }
-                }.navigationBarTitle("New Wyre", displayMode: .inline)
-            .navigationBarItems(leading:
-            Button(action: {
-                self.mode.wrappedValue.dismiss()
-                }) {
-                    Image(systemName: "xmark").font(.system(size: 22, weight: .semibold)).foregroundColor(Color.white)
-                }
-            )
+                        
+                        
+                        
+                            NavigationLink(destination: Text("Privacy Options")) {
+                                HStack{
+                                    Image(systemName: "lock").font(.system(size: 30, weight: .semibold)).padding()
+                                    VStack(alignment: .leading, spacing: 4.0){
+                                        Text("Privacy Options").font(.custom("Gotham-Bold" ,size: 14))
+                                        Text("Private").font(.custom("Gotham-Book" ,size: 14))
+                                    }
+                                }
+                                }.frame(height: 75)
+                    }
+                    Button(action: {print("hello")}) {
+                        Text("Next").font(.custom("Gotham-Bold" ,size: 16)).multilineTextAlignment(.center).padding().foregroundColor(ColorManager.wyrePurple)
+                    }
+                    }.navigationBarTitle("New Wyre", displayMode: .inline)            .navigationBarHidden(true)
+                    .navigationBarBackButtonHidden(true)
+            }
+
         }
         
 
